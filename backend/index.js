8b285a306e26dd0f16de79927a1b60db5259b97c
Sujal Rawat <108566302+sujalrawat@users.noreply.github.com>
2024-08-08 17:43:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 //middlewares
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://deploy-mern-client-liard.vercel.app",
     credentials:true
 }))
 app.use(cookieParser())
